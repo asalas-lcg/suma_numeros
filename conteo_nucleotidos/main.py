5f2a5b195a6ffa17c79fda07f224597c55be0236
AdaraSalas <adarasalasmanzo@gmail.com>
2026-04-16 10:19:41 -0600

    mayusculas

--- conteo_nucleotidos/main.py
+++ conteo_nucleotidos/main.py
@@ -1,9 +1,9 @@
 # 1. Leer secuencia
 secuencia = input("Ingrese la secuencia de AND: ")
-print("Secuencia ingresada:", secuencia)
 
 # 2. Convertir a mayúsculas
 secuencia = secuencia.upper()
+print("Secuencia ingresada:", secuencia)
 
 # 3. Validar si está vacía
 # 4. Validar caracteres
