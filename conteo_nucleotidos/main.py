b681033b673f7df9eb5ee904390b135853fd0847
AdaraSalas <adarasalasmanzo@gmail.com>
2026-04-16 10:27:41 -0600

    resultados

--- conteo_nucleotidos/main.py
+++ conteo_nucleotidos/main.py
@@ -8,4 +8,12 @@ print("Secuencia ingresada:", secuencia)
 # 3. Validar si está vacía
 # 4. Validar caracteres
 # 5. Contar nucleótidos
+count_a = secuencia.count("A")
+count_t = secuencia.count("T")
+count_c = secuencia.count("C")
+count_g = secuencia.count("G")
+print("Número de adeninas (A):", count_a)
+print("Número de timinas (T):", count_t)
+print("Número de citosinas (C):", count_c)
+print("Número de guaninas (G):", count_g)
 # 6. Mostrar resultados
